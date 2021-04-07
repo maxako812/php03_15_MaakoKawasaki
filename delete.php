34b47f2a0e3c5cef54c43d0684003e42d0a784c4
MaakoKawasaki <maxako812@gmail.com>
2021-04-07 15:01:42 +0900

    画像ファイル削除実装完了

--- delete.php
+++ delete.php
@@ -5,13 +5,37 @@ $id   = $_GET['id'];
 require_once('funcs.php');
 // DBに繋ぐ関数の呼び出し
 $pdo = connectDB();
-$sql = "DELETE FROM gs_bm_table WHERE id = :id;";
-$stmt = $pdo->prepare($sql);
-
+// ①画像ファイル削除処理：まずは画像のファイル名を呼び出す
+$sql_select = 'SELECT image FROM gs_bm_table WHERE id = :id';
+$stmt = $pdo->prepare($sql_select);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $status = $stmt->execute(); //実行
 
-//４．データ登録処理後
+if ($status == false) {
+    sql_error($status);
+} else {
+    $result = $stmt->fetch();
+}
+
+$filepath = "./images/" . $result['image'];
+// pathにファイルが存在すれば削除する
+if(file_exists($filepath)){
+    unlink($filepath);
+}
+
+// ②DB削除処理
+
+$sql_delete = "DELETE FROM gs_bm_table WHERE id = :id;";
+$stmt = $pdo->prepare($sql_delete);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+$status = $stmt->execute(); //実行
+
+//画像ファイルの削除
+
+
+
+// SQLエラーチェック
 if ($status == false) {
     sql_error($stmt);
 } else {
